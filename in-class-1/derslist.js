2978c44bd3b61e9f55b5074df3c3bece22c6f0a3
Muhammed Eroğlu <llofmejb@gmail.com>
2022-01-31 00:55:31 +0300

    1

---
+++ in-class-1/derslist.js
@@ -0,0 +1,28 @@
+1     Intro @ Roy-Instructor                  31.07.2021Page
+2     JSX - Components @ John-Instructor      02.08.2021Page
+3     Styling @ Roy-Instructor                07.08.2021Page
+4     Events @ Roy-Instructor                 09.08.2021Page
+5     Lifecycle @ John S. -Instructor         16.08.2021Page
+6     Hooks@ Oliver - Instructor              18.08.2021Page
+7     Recap @ Cedric                          21.08.2021Page
+8     Recap Lifecycle@ John S.                22.08.2021Page
+9     Router @ John S.                        28.08.2021Page
+10    Hooks@ Oliver                           01.09.2021Page
+11    Styled Comp. - Material UI@Roy          04.09.2021Page
+12    Context@Oliver                          04.09.2021Page
+13    Redux @John                             11.09.2021Page
+14    Fire contact@Roy                        11.09.2021Page
+15    Thunk User@John                         13.09.2021Page
+16    Register Form Validation - Milestone    16.09.2021Page 
+
+
+
+1     Tour Project @ Oliver                   14.08.2021Page
+2     Language Cards Project @ Alper          24.08.2021Page
+3     Task Tracker @Roy                       26.08.2021Page
+4     Recipe App @Oliver                      08.09.2021Page
+5     Movie App @John                         09.09.2021Page
+6     Blog App - 1 @Noah Adams                30.09.2021Page
+7     Blog App - 2 @Noah Adams                02.10.2021Page
+8     Blog App - 3 @Noah Adams                02.10.2021Page
+9     Blog App - 4 @Noah Adams                05.10.2021Page
