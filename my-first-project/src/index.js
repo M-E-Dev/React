940df2903f91d98566b6f0f949c6cf4c7ebf766f
Muhammed Eroğlu <llofmejb@gmail.com>
2022-04-17 15:36:23 +0300

    3

--- my-first-project/src/index.js
+++ my-first-project/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// import React, { Component } from 'react';
+// bunu alırsak extends React.Component yerine Component yazmamız yeterli olur
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
