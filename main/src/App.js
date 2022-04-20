4e807f58ca5fb2fdf9de06c639bb0718a7334911
Muhammed Eroğlu <llofmejb@gmail.com>
2022-04-20 17:24:30 +0300

    1

--- main/src/App.js
+++ main/src/App.js
@@ -1,18 +1,25 @@
-import logo from "./logo.svg";
-import "./App.css";
-import ClassComponent from "./components/ClassComponent";
-import FunctionalComponent from "./components/FunctionalComponent";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hogwarts App</h1>
-      <ClassComponent name="Draco"/>
-      <FunctionalComponent name="Harry" />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
-
-
