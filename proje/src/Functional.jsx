a714d57dd3349a9bb73d45ab217d15139ebc6d0d
Muhammed Eroğlu <llofmejb@gmail.com>
2022-04-28 15:47:28 +0300

    3

---
+++ proje/src/Functional.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Functional() {
+    // class olmadığı için dot, this vs gerekmez. func içindekini kullnabiliriz doğrudan..
+  return (
+    <div>Functional</div>
+  )
+}
