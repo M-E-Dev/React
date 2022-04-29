1c570c2d66b6a4c6ab6e16c390ddd7d8e55b2934
Muhammed Eroğlu <llofmejb@gmail.com>
2022-04-29 14:15:49 +0300

    2

--- proje/src/App.js
+++ proje/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import ClassBased from "./ClassBased";
+import Functional from "./Functional";
 
 function App() {
     return(
         <div className="App">
             <ClassBased name="Harry"/>
+            <br/><hr/><br/>
+            <Functional name="Harry"/>
         </div>
     )
 }
--- proje/src/Functional.jsx
+++ proje/src/Functional.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function Functional() {
-    // class olmadığı için dot, this vs gerekmez. func içindekini kullnabiliriz doğrudan..
+export default function Functional(props) {
+  // class olmadığı için dot, this vs gerekmez. func içindekini kullnabiliriz doğrudan..
+  // this classa has bişey. state objesi de yok, dot nota. yok
+
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count+1)
+  }
+
+  const decrement = () => {
+    if (count>0) {
+      setCount(count-1)
+    }
+  }
+
   return (
-    <div>Functional</div>
-  )
+    <div>
+      <button onClick={increment}>+</button>
+      <h1>{count}</h1>
+      <button onClick={decrement} >-</button>
+      <h2>{props.name}</h2>
+    </div>
+  );
 }
