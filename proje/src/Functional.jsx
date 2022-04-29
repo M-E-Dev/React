962a2eb6410536699b0c82d7c37082a75958f968
Muhammed Eroğlu <llofmejb@gmail.com>
2022-04-29 16:06:14 +0300

    3

--- proje/src/Functional.jsx
+++ proje/src/Functional.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Functional(props) {
   // class olmadığı için dot, this vs gerekmez. func içindekini kullnabiliriz doğrudan..
   // this classa has bişey. state objesi de yok, dot nota. yok
 
   const [count, setCount] = useState(0);
+  const [empty, setEmpty] = useState(0);
 
   const increment = () => {
     setCount(count+1)
@@ -15,6 +16,15 @@ export default function Functional(props) {
       setCount(count-1)
     }
   }
+
+  // dependency yoksa her update
+  // dependency varsa sadece mount
+  // return varsa unmount
+  useEffect( () => {
+    console.log("useEffect");
+    // // useEffect içinde state değiştirirsek sonsuz döngüye girer... infinit loop...
+    // setEmpty(empty+1)
+  })
 
   return (
     <div>
