e1dd665eeb51a257296c418771bfaad73d323914
Muhammed Eroğlu <llofmejb@gmail.com>
2022-05-01 19:02:23 +0300

    2

--- proje/src/App.js
+++ proje/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import Counter from './Counter';
+import Recap from './Recap';
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+  }
   render() {
     return (
       <div className="App">
-      <h2>My Counter</h2>
-      <Counter/>
+      {/* <h2>My Counter</h2>
+      <Counter/> */}
+      <Recap name="Harry"/>
+
     </div>
     )
   }
--- proje/src/recap.js
+++ proje/src/recap.js
@@ -4,14 +4,39 @@ export default class Recap extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        name: "Constructor Method"
+      name: "Hermione",
+      data: [],
     };
   }
+
+  // // propsu alıp state atamak yerine aşağıdaki yöntem
+  // // state deki name'e propstaki name atandı
+  // static getDerivedStateFromProps(props, state) {
+  //   // state döndürmeyeceksek ---return null--- veririz
+  //   return { name: props.name };
+  // }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => this.setState({ data: data }));
+
+      this.setState({name: "component should update true"})
+  }
+
+
+  shouldComponentUpdate(){
+    return false; // true yaparsak update olur
+  }
+
 
   render() {
     return (
       <div>
         <p>This is {this.state.name}</p>
+        <p>My name is {this.props.name}</p>
+        {/* key olarak unique olan herhangi bieşey kullanılabilir... */}
+        {this.state.data.map(item => <h6 key={item.id}>{item.id} - {item.name}</h6>)}
       </div>
     );
   }
