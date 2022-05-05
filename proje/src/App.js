47ee40f14b85e03ce166b7be441665a3c318b535
Muhammed Eroğlu <llofmejb@gmail.com>
2022-05-05 21:22:32 +0300

    2

--- proje/src/App.js
+++ proje/src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
+import "./App.css";
 import Header from "./component/Header";
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
-export default function App() {
+import { BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom";
+
+const App = () => {
   return (
-    <Router>
+    <Router basename="/hogwarts">
+    {/* <Router forceRefresh ???> */}
       <div className="App">
         <Header />
       </div>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="about" element={<About/>} />
+        <Route path="profile" element={<Profile/>} />
+      </Routes>
     </Router>
   );
-}
+};
+
+export default App;
--- proje/src/component/Header.js
+++ proje/src/component/Header.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div>
+      <h2>Header</h2>
+      <ul className="nav">
+        <li>
+        {/* <a href="/" >Home</a>  RENDER ALIYORDU  */}
+          <Link to="/" >Home</Link>
+        </li>
+        <li>
+          <Link to="/about" >About</Link>
+        </li>
+        <li>
+          <Link to="/profile" >Profile</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
