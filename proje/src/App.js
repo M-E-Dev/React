60d845d9378b4f2c2111810253c0576b30c3f770
Muhammed Eroğlu <llofmejb@gmail.com>
2022-05-06 09:52:11 +0300

    3

--- proje/src/App.js
+++ proje/src/App.js
@@ -11,7 +11,6 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Switch,
 } from "react-router-dom";
 
 const App = () => {
--- proje/src/pages/Post.js
+++ proje/src/pages/Post.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
-const Post = ({match}) => {
+const Post = () => {
+
+  const params = useParams();
+
   return (
     <div>
-        <h2>Post name: {match.params.name}</h2>
+      <h2>Post name: {JSON.stringify(params.name)}</h2>
     </div>
-  )
-}
+  );
+};
 
 export default Post;
-
-------------------------------------DERS 2,12---------------------------------------------
