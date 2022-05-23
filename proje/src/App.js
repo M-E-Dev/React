88c47eefef3be80f25d6917d2d8d6ac8a3110215
Muhammed Eroğlu <llofmejb@gmail.com>
2022-05-23 13:47:25 +0300

    3

--- proje/src/App.js
+++ proje/src/App.js
@@ -5,7 +5,8 @@ import aws from "./assets/aws.png";
 import { StyledButton1, StyledButton2 } from "./components/styled-comp/button/Button.styles";
 import Wrapper from "./components/styled-comp/wrapper/Wrapper/Wrapper";
 import HeaderText from "./components/styled-comp/header/HeaderText";
-
+import Link from "./components/styled-comp/link/link";
+import ExtendedComp from "./components/styled-comp/extended-link/ExtendedComp";
 
 function App() {
   const [counter, setCounter] = useState(0);
@@ -39,6 +40,10 @@ function App() {
       <StyledButton2 dark>Dark</StyledButton2>
       <Body img={img} />
       <HeaderText>Hogwarts</HeaderText>
+      {/* // link rel="noopener" "noreferer" nedir? */}
+      <Link href="https://www.instagram.com/" target="_blank" rel="noopener">INSTAGRAM</Link>
+      <Link href="https://www.instagram.com/" target="_blank" rel="noopener" secondary>FACEBOOK</Link>
+      <ExtendedComp >FACEBOOK</ExtendedComp>
 
     </Wrapper>
   );
--- proje/src/components/styled-comp/link/link.js
+++ proje/src/components/styled-comp/link/link.js
@@ -1,8 +1,20 @@
 import styled, { css } from "styled-components";
 
-// CSS & ~ & + . vs araştır
+// CSS & ~ & + . vs araştır Pseudoelements  https://styled-components.com/docs/basics#pseudoelements-pseudoselectors-and-nesting
 
-export styled.a`
+export default styled.a`
+background-color: black;
 font-size: 1.2rem;
 transition: ease 0.25s all;
+text-decoration: none;
+color: blanchedalmond;
+${props => props.secondary && css`
+background-color: blue;
+border-radius: 8px;
+padding: 8px;
+`}
+&:hover {
+    font-size: 1.5rem;
+    opacity: 0.6;
+}
 `
