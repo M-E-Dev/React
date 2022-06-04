363c38c99f9a8909d6bdbbd3b2591dd0ab1da8f3
Muhammed Eroğlu <llofmejb@gmail.com>
2022-06-04 18:02:08 +0300

    3

--- proje/src/App.js
+++ proje/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useReducer } from "react";
+import { reducer } from "./reducer";
 
 const initialState = {
   data:"",
@@ -8,25 +9,32 @@ const initialState = {
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
+  const {data, loading, error} = state;
+
+
 
   // const [data, setData] = useState("");
   // const [loading, setLoading] = useState(false);
   // const [error, setError] = useState("");
 
   const fetchData = () => {
-    setLoading(true);
-    setError("");
-    setData("");
+
+    dispatch({type: "FETCH_START"})
+    // setLoading(true);
+    // setError("");
+    // setData("");
 
     fetch("https://api.thecatapi.com/v1/images/search")
       .then((res) => res.json())
       .then((res) => {
-        setLoading(false);
-        setData(res[0].url);
+        dispatch({type: "FETCH_SUCCESS", payload: res[0].url})
+        // setLoading(false);
+        // setData(res[0].url);
       })
       .catch(() => {
-        setLoading(false);
-        setError("Something went wrong");
+        dispatch({type: "FETCH_FAIL", payload: "Something went wrong"})
+        // setLoading(false);
+        // setError("");
       });
   };
 
