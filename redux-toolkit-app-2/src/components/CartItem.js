2e1da46512249aa9521f212279ac0b7426eff8e6
Muhammed Eroğlu <llofmejb@gmail.com>
2022-09-26 20:57:22 +0300

    2

--- redux-toolkit-app-2/src/components/CartItem.js
+++ redux-toolkit-app-2/src/components/CartItem.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
 const CartItem = () => {
-  const { cartItems, total, amount } = useSelector((store) => store.cart);
-  if (amount < 1) {
-    return (
-      <section className="cart">
-        <header>
-          <h2>your bag</h2>
-          <h4 className="empty-cart">is currently empty</h4>
-        </header>
-      </section>
-    );
-  }
+  return (
+    <div>
+      cart item
+    </div>
+  )
 };
 
 export default CartItem;
